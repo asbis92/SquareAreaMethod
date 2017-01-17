1e9f6f4a53c5ba7f5b8af43cd739e272b7abce94
asbis92 <rdsl_ivanov@yahoo.com>
2017-01-17 18:18:03 +0200

    A simple console program for calculation of a square area

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareAreaMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine(SquareArea(a));
+        }
+        static double SquareArea(double sideA) {
+            double area = sideA * sideA;
+            return area;
+        }
+    }
+}
